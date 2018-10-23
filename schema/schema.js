595c30ed15e61537e4acdefa7bd2979238f0fcf5
David Porras <david.porras12@gmail.com>
2018-10-23 17:31:24 -0300

    @GraphQL - Nesting queries

--- schema/schema.js
+++ schema/schema.js
@@ -2,12 +2,33 @@ const axios = require('axios');
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve (parentValue, args) {
+        return axios.get(`http://localhost:6000/companies/${parentValue.companyId}`).then(response => response.data);
+      }
+    }
   }
 });
 
